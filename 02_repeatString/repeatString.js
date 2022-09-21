7537b48d5cccb01b7de2bf0dcd5a64192f547c9c
KevinZer00 <58315100+KevinZer00@users.noreply.github.com>
2022-09-20 21:36:06 -0700

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,7 @@
 const repeatString = function(string, num) {
-  var returnString = " ";
   for (i = 0; i < num; i++)
   {
-    returnString = returnString += string;
+    let returnString = returnString += string;
   }
   return returnString;
 }
